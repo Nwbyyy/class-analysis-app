e9f1ebb64e5c80c133757ca4340afb2733ee8c0e
KimAllise32 <114198322+KimAllise32@users.noreply.github.com>
2023-09-28 17:42:26 -0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+def main():
+  # call all the functions here 
+  print("Make sure your data is in an excel sheet in the same directory as this file")
+  print("Please enter the name of your excel sheet")
+  file_name = input()
+  read_file(file_name)
