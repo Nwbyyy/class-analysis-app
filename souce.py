61f56ab95164920d7652bde8c8400943f356d687
KimAllise32 <114198322+KimAllise32@users.noreply.github.com>
2023-09-28 17:35:26 -0400

    source.py

--- souce.py
+++ souce.py
@@ -1,1 +1,12 @@
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#from sklearn.preprocessing import StandardScaler
+#from imblearn.over_sampling import RandomOverSampler
 
+#pip install xlrd
+
+
+def read_file():
+    ds = pd.read_excel("coding.xlsx")
+    ds.head()
